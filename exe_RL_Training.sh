fd26660bdca96e7735f0c3d3d9fc5f1ec8338700
SubAnoy <wty0107@126.com>
2023-10-30 00:52:25 +0000

    update

--- exe_RL_Training.sh
+++ exe_RL_Training.sh
@@ -49,17 +49,3 @@ python3 ./scripts/RL_controller_offline.py --RL_policy DDPG --load_model default
 
 
 
-echo "-----------------------------------------------------------------------------------------"
-
-echo "Start Testing......"
-
-echo "RL_stream_Controller"
-
-#Simple O2 system design
-python3 ./scripts/RL_stream_controller.py --RL_policy DDPG --data_file $file_name --load_model default --Index $2 --seed $seed_value --query_type $1
-
-#Full O2 system design
-# python3 ./scripts/RL_O2.py --RL_policy DDPG --data_file $file_name --load_model default --Index $2 --seed $seed_value --query_type $1
-
-
-echo "End Testing......"
